9dc1769f0fc6db156103378c7f48f6fdbf6cd2ce
Sharon Abramson <sharabramson@gmail.com>
2023-11-16 15:13:43 -0500

    re-link stylesheet and javascript path. worked on generatepassword method - created objects with keys and values

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,20 +11,53 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-var  passLength = window.prompt("Choose a password length between 8 and 128");
-console.log(passLength);
-
-if (passLength <8 || passLength >128) {
-  alert("You must choose a number between 8 and 128!");
-  window.open("index.html");
-}
-
-passLength = window.prompt("which character types would you like to include?");
+// function writePassword();
 
 
+//  var index = Math.floor(Math.random() * options.length);
+// var computerChoice = options[index];
 
-function generatePassword(){
+function generatePassword() {
   //your code goes here...
-}
+let charTotal = []
 
+  let charSets = {
+    lowerCase: "abcdefghijklmnopqrstuvwxyz",
+    upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    numbers: "1234567890",
+    specialChars: "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+  }
+
+  let upperCase = window.confirm("include uppercase letters?")
+  if (upperCase) {
+    charTotal = charSets.upperCase;
+  }
+
+  let lowerCase = window.confirm("include lowercase letters?")
+  if (lowerCase) {
+    charTotal += charSets.lowerCase;
+  }
+
+  let numbers = window.confirm("include numbers?")
+  if (numbers) {
+    charSets = charSets.concat(numeric);
+  }
+
+  let specialChars = window.confirm("include special characters?")
+  if (specialChars) {
+    charSets = charSets.concat(specialChars);
+  }
+
+  var passLength = window.prompt("Choose a password length between 8 and 128");
+
+  var index = Math.floor(Math.random() * charSets.length);
+
+
+  if (passLength < 8 || passLength > 128) {
+    alert("You must choose a number between 8 and 128!");
+    window.open("index.html");
+  }
+
+  passLength = window.prompt("which character types would you like to include?");
+
+}
