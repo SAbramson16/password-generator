7cce04cecd62d75390526f320534c6a2099254c2
Sharon Abramson <sharabramson@gmail.com>
2023-11-16 23:57:03 -0500

    included pages URL in README, completed checkCriteria function, completed generate function and completed generatePassword to return the randomized password. added comments into script file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Assignment Code
+// Assignment Code - MAIN START
 var generateBtn = document.querySelector("#generate");
 
 
@@ -7,22 +7,75 @@ generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 }
 
+//this method uses the charSets object and a criteria object to select the criteria based on the users choices. 
+//this method returns a list made of all the user selected criteria lists.
+function checkCriteria(charSets, criteria) {
+  let charTotal = [];
+  
+  window.alert("Please choose at least 1 of the following criteria for your password.");
+ 
+  criteria.isUppercase = window.confirm("Include uppercase letters?");
+  if (criteria.isUppercase) {
+    //used console.log for debugging
+    // console.log("upper: " + charSets.upperCase);
+    charTotal = charTotal.concat(charSets.upperCase);
+  }
 
+  criteria.isLowercase = window.confirm("Include lowercase letters?");
+  if (criteria.isLowercase) {
+    //used console.log for debugging
+    // console.log("lower: " + charSets.lowerCase);
+    charTotal = charTotal.concat(charSets.lowerCase);
+  }
+
+  criteria.isNumbers = window.confirm("Include numbers?");
+  if (criteria.isNumbers) {
+    //used console.log for debugging
+    // console.log("num: " + charSets.numbers); 
+    charTotal = charTotal.concat(charSets.numbers);
+  }
+
+  criteria.isSpecial = window.confirm("Include special characters?");
+  if (criteria.isSpecial) {
+    //used console.log for debugging
+    // console.log("special: " + charSets.specialChars);
+    charTotal = charTotal.concat(charSets.specialChars);
+  }
+
+  criteria.passLength = window.prompt("Choose a password length between 8 and 128");
+
+  if (criteria.passLength < 8 || criteria.passLength > 128) {
+    alert("You must choose a number between 8 and 128!");
+    charTotal = [];
+  }
+
+   return charTotal;
+}
+
+//this method use the criteriaList and the password length to generate a random password.
+function generate(criteriaList, passLength) {
+  let password = "";
+  for (let i = 0; i < passLength; i++) {
+    let random_index = Math.floor(Math.random() * criteriaList.length)
+    password += criteriaList[random_index];
+  }
+  return password;
+}
+
+// this method will generate a password based on a specific user selected criteria list. this method will return the generated password.
 function generatePassword() {
-  //your code goes here...
-  let charTotal = []
-
+  // this function created the charSets and criteria objects with keys and values.
   let charSets = {
     lowerCase: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
     upperCase: ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
-    numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, ],
-    specialChars: [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"],
+    numbers: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    specialChars: [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "\`", "{", "|", "}", "~"],
   }
 
   let criteria = {
@@ -30,81 +83,19 @@ function generatePassword() {
     isLowercase: false,
     isNumbers: false,
     isSpecial: false,
+    passLength: 0
   }
 
-  function checkCriteria(charSets, criteria) {
-  
-    window.alert("Please choose at least 1 of the following criteria for your password. Click okay for yes and cancel for no.");
-   
-    criteria.isUppercase = window.confirm("Include uppercase letters?")
-    if (criteria.isUppercase) {
-      charTotal += charSets.upperCase;
-    }
-  
-    criteria.isLowercase = window.confirm("Include lowercase letters?")
-    if (criteria.isLowercase) {
-      charTotal += charSets.lowerCase;
-    }
-  
-    criteria.isNumbers = window.confirm("Include numbers?")
-    if (criteria.isNumbers) {
-      charTotal += charSets.numbers;
-    }
-  
-    criteria.isSpecial = window.confirm("Include special characters?")
-    if (criteria.isSpecial) {
-      charTotal += charSets.specialChars;
-    }
-  
-    let passLength = window.prompt("Choose a password length between 8 and 128");
-  
-    if (passLength < 8 || passLength > 128) {
-      alert("You must choose a number between 8 and 128!");
-      window.open("index.html");
-    }
-  
-    // return;
-  }
-  password = ""
-  for (let i = 0; i < charTotal.Length; i++) {
-    password += charTotal[i](Math.floor(Math.random() * "charTotal"));
-  }
-console.log(checkCriteria);
-  
-  // var index =  * charTotal.length);
-
-  let totalCritieriaList = checkCriteria(charSets, criteria);
-  
-
-
-
-  
-  if (charTotal.length == 0)
+  // made totalCriteriaList by passing charSets and criteria through checkCriteria function, which uses charSets to make a random list.
+  let totalCriteriaList = checkCriteria(charSets, criteria);
+  // if the user doesn't select any criteria, (the totalCriteriaList is empty) they will be brought back to the beginning. 
+  if (totalCriteriaList.length == 0)
   {
-    alert("You must select at least 1 criteria!")
-    return;
+    alert("You must select at least 1 criteria and password length must be between 8 and 128!");
+    window.open("index.html");
   }
 
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- 
-
-  
+  //this function uses the totalCriteriaList and the password length chosen and generates the password.
+  let password = generate(totalCriteriaList, criteria.passLength);
+  return password;
 }
